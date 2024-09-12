b6bf1387b15a5b0d6dad53dd6eb6a148e85fb0f2
nanakyeong <dlskrud468@naver.com>
2024-09-12 15:59:37 +0900

    feat : 브랜치 생성 및 mysql 설정

---
+++ src/main/java/com/example/planj/PlanJApplication.java
@@ -0,0 +1,13 @@
+package com.example.planj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanJApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanJApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/planj/PlanJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanJApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
