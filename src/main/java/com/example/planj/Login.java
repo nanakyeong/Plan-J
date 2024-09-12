57a8947444fe2252097bd22942b1cd822f96d8f1
nanakyeong <dlskrud468@naver.com>
2024-09-12 16:36:05 +0900

    feat : 로그인 페이지 생성

---
+++ src/main/java/com/example/planj/Login.java
@@ -0,0 +1,34 @@
+package com.example.planj;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Login extends JFrame {
+    public Login() {
+        setTitle("Plan J");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1000,600);
+
+        Container contentPane = getContentPane();
+        contentPane.setLayout(null);
+
+        JLabel logo = new JLabel("Plan J");
+        logo.setBounds(460, 10, 100, 20);
+        contentPane.add(logo);
+
+        JButton check = new JButton("확인");
+        check.setBounds(450,500,100,20);
+        contentPane.add(check);
+
+        JTextField id = new JTextField("아이디");
+        id.setBounds(450,100,100,20);
+        contentPane.add(id);
+        id.setColumns(10);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Login login = new Login();
+    }
+}
