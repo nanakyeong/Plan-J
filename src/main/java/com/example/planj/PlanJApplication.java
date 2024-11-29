a1ff3d331491923439c37ca0aa01b3d1f516c15a
newronkim <144310846+newronkim@users.noreply.github.com>
2024-11-30 04:01:28 +0900

    feat: 삭제 멘트 수정

--- src/main/java/com/example/planj/PlanJApplication.java
+++ src/main/java/com/example/planj/PlanJApplication.java
@@ -1,10 +1,8 @@
 package com.example.planj;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.swing.*;
